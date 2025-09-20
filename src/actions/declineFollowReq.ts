d5c7db8c092e98d56e19cbc8fc384169c03a1768
gogagureshidze <94310590+gogagureshidze@users.noreply.github.com>
2025-09-20 05:15:01 +0100

    fixed caching bugs in whole codebase

--- src/actions/declineFollowReq.ts
+++ src/actions/declineFollowReq.ts
@@ -1,7 +1,7 @@
 "use server";
 import prisma from "@/lib/client";
 import { currentUser } from "@clerk/nextjs/server";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 async function acceptFollowReq(userId: string) {
   const user = await currentUser();
@@ -25,7 +25,9 @@ async function acceptFollowReq(userId: string) {
         },
       });
     }
-
+revalidateTag("user-relationships"); // refresh profile buttons
+    revalidateTag("feed-posts");
+    revalidateTag("user-profile");
     revalidatePath("/requests");
 
   } catch (error) {
