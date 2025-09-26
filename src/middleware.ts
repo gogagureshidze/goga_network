cc28401567fe99146a601b0d8e3a2a7fa0e5c2ff
gogagureshidze <94310590+gogagureshidze@users.noreply.github.com>
2025-09-27 00:26:59 +0100

    socket server update and mainchat conection update and context for socket conection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// middleware.ts
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-
-const isProtectedRoute = createRouteMatcher(["/settings(.*)"])
-
-export default clerkMiddleware((auth, req) => {
-     if(isProtectedRoute(req)) auth.protect()
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  // Protect /settings and all its subpaths
+  matcher: ["/settings/:path*"],
 };
